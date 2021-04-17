327f36f7bf866da922779fd089786648759ba41f
Cigan <darknew1998@mail.ru>
2021-04-17 12:57:35 +0700

    [FIX]: Logic template props error

--- src/utils/Shared/Templates/ReactLogic.template.ts
+++ src/utils/Shared/Templates/ReactLogic.template.ts
@@ -3,4 +3,4 @@ export const logicTemplate = (name: string) => `import React from 'react';
 interface I${name}LogicProps {
 }
 
-export const L${name} = (): I${name}LogicProps => {}`;
+export const L${name} = (props: I${name}LogicProps) => {}`;
