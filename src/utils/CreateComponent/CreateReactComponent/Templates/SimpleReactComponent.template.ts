c32b660a8b0e1e535eb6ade1f8817d82945219e6
Cigan <makuizafailed@gmail.com>
2023-08-21 21:11:01 +0700

    [FIX] small fix in component template

--- src/utils/CreateComponent/CreateReactComponent/Templates/SimpleReactComponent.template.ts
+++ src/utils/CreateComponent/CreateReactComponent/Templates/SimpleReactComponent.template.ts
@@ -9,11 +9,7 @@ export const simpleReactComponentTemplate = (
     const interfaceName =
         exportInterface === EYesOrNo.yes ? `I${name}Props` : 'IProps';
     return `import React from 'react';
-${
-    styled === EYesOrNo.yes
-        ? `import { S${name} } from './${name}.styled.ts'`
-        : ''
-}
+${styled === EYesOrNo.yes ? `import { S${name} } from './${name}.styled'` : ''}
 
 ${exportInterface === EYesOrNo.yes ? 'export ' : ''}interface ${interfaceName} {
     className?: string;
@@ -22,14 +18,22 @@ ${exportInterface === EYesOrNo.yes ? 'export ' : ''}interface ${interfaceName} {
 ${
     reactMemo === EYesOrNo.yes
         ? `
-export const ${name} = React.memo<${interfaceName}>(() => {
+export const ${name} = React.memo<${interfaceName}>(({className}) => {
     return (
-        ${styled === EYesOrNo.yes ? `<S${name}></S${name}>` : `<div></div>`}
+        ${
+            styled === EYesOrNo.yes
+                ? `<S${name} className={className}></S${name}>`
+                : `<div className={className}></div>`
+        }
     )
 });`
-        : `export const ${name}: React.FC<${interfaceName}> = () => {
+        : `export const ${name}: React.FC<${interfaceName}> = ({className}) => {
         return (
-            ${styled === EYesOrNo.yes ? `<S${name}></S${name}>` : `<div></div>`}
+            ${
+                styled === EYesOrNo.yes
+                    ? `<S${name} className={className}></S${name}>`
+                    : `<div className={className}></div>`
+            }
         )
 };`
 }`;
